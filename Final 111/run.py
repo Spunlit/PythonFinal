f6527cb68d63b74021c3ded3d2a07fa92c4b983d
Spunlit <150501545+Spunlit@users.noreply.github.com>
2024-11-17 19:41:11 +0500

    Add files via upload

--- Final 111/run.py
+++ Final 111/run.py
@@ -1,8 +1,10 @@
+from flask import Flask
+
 from app import create_app
 import os
 
 app = create_app()
-
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
